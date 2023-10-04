03c26d7888ad0606fadc27d2f578d52f9d31141c
Faraax <suny.faraz@gmail.com>
2023-10-04 21:20:17 +0500

    Transaction Page Done

--- app/transaction/page.jsx
+++ app/transaction/page.jsx
@@ -14,6 +14,7 @@ export default function page() {
                     <h2 className="text-mute text-4xl">Tommorow</h2>
                 </div>
             </div>
+
             <div className="grid grid-cols-3 px-10 gap-7">
                 <div className="flex gap-5 flex-col">
                     <div className="flex items-center w-full">
@@ -41,6 +42,27 @@ export default function page() {
                             />
                         </div>
                     </div>
+                    <div className="bg-white shadow-xl rounded-lg p-5 flex flex-col gap-3">
+                        <span className="bg-orange-300 w-full h-2 rounded-xl" />
+                        <h3 className="text-2xl">Date of Infographic</h3>
+                        <p className="text-mute text-xl">July 14</p>
+                        <div className="flex gap-2">
+                            <Image
+                                height={'auto'}
+                                width={'auto'}
+                                src={basharAllan}
+                                className='rounded-full object-cover aspect-square w-12 h-12'
+                                alt='review'
+                            />
+                            <Image
+                                height={'auto'}
+                                width={'auto'}
+                                src={ghassanKhoulaghasi}
+                                className='rounded-full object-cover aspect-square w-12 h-12'
+                                alt='review'
+                            />
+                        </div>
+                    </div>
                 </div>
 
                 <div className="flex gap-5 flex-col">
@@ -49,7 +71,28 @@ export default function page() {
                         <HiDotsHorizontal className="ml-auto mr-20 text-mute text-2xl" />
                     </div>
                     <div className="bg-white shadow-xl rounded-lg p-5 flex flex-col gap-3">
-                        <span className="bg-blue-300 w-full h-2 rounded-xl" />
+                        <span className="bg-orange-300 w-full h-2 rounded-xl" />
+                        <h3 className="text-2xl">Date of Infographic</h3>
+                        <p className="text-mute text-xl">July 14</p>
+                        <div className="flex gap-2">
+                            <Image
+                                height={'auto'}
+                                width={'auto'}
+                                src={basharAllan}
+                                className='rounded-full object-cover aspect-square w-12 h-12'
+                                alt='review'
+                            />
+                            <Image
+                                height={'auto'}
+                                width={'auto'}
+                                src={ghassanKhoulaghasi}
+                                className='rounded-full object-cover aspect-square w-12 h-12'
+                                alt='review'
+                            />
+                        </div>
+                    </div>
+                    <div className="bg-white shadow-xl rounded-lg p-5 flex flex-col gap-3">
+                        <span className="bg-purple-300 w-full h-2 rounded-xl" />
                         <h3 className="text-2xl">Date of Infographic</h3>
                         <p className="text-mute text-xl">July 14</p>
                         <div className="flex gap-2">
@@ -77,7 +120,91 @@ export default function page() {
                         <HiDotsHorizontal className="ml-auto mr-20 text-mute text-2xl" />
                     </div>
                     <div className="bg-white shadow-xl rounded-lg p-5 flex flex-col gap-3">
+                        <span className="bg-purple-300 w-full h-2 rounded-xl" />
+                        <h3 className="text-2xl">Date of Infographic</h3>
+                        <p className="text-mute text-xl">July 14</p>
+                        <div className="flex gap-2">
+                            <Image
+                                height={'auto'}
+                                width={'auto'}
+                                src={basharAllan}
+                                className='rounded-full object-cover aspect-square w-12 h-12'
+                                alt='review'
+                            />
+                            <Image
+                                height={'auto'}
+                                width={'auto'}
+                                src={ghassanKhoulaghasi}
+                                className='rounded-full object-cover aspect-square w-12 h-12'
+                                alt='review'
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex gap-5 flex-col">
+                    <div className="flex items-center w-full">
+                        <h2 className="text-xl">Downloads</h2>
+                        <HiDotsHorizontal className="ml-auto mr-20 text-mute text-2xl" />
+                    </div>
+                    <div className="bg-white shadow-xl rounded-lg p-5 flex flex-col gap-3">
                         <span className="bg-blue-300 w-full h-2 rounded-xl" />
+                        <h3 className="text-2xl">Date of Infographic</h3>
+                        <p className="text-mute text-xl">July 14</p>
+                        <div className="flex gap-2">
+                            <Image
+                                height={'auto'}
+                                width={'auto'}
+                                src={basharAllan}
+                                className='rounded-full object-cover aspect-square w-12 h-12'
+                                alt='review'
+                            />
+                            <Image
+                                height={'auto'}
+                                width={'auto'}
+                                src={ghassanKhoulaghasi}
+                                className='rounded-full object-cover aspect-square w-12 h-12'
+                                alt='review'
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex gap-5 flex-col">
+                    <div className="flex items-center w-full mt-7">
+                        {/* <h2 className="text-xl">In Progress</h2> */}
+                        {/* <HiDotsHorizontal className=" ml-auto mr-20 text-mute text-2xl" /> */}
+                    </div>
+                    <div className="bg-white shadow-xl rounded-lg p-5 flex flex-col gap-3">
+                        <span className="bg-orange-300 w-full h-2 rounded-xl" />
+                        <h3 className="text-2xl">Date of Infographic</h3>
+                        <p className="text-mute text-xl">July 14</p>
+                        <div className="flex gap-2">
+                            <Image
+                                height={'auto'}
+                                width={'auto'}
+                                src={basharAllan}
+                                className='rounded-full object-cover aspect-square w-12 h-12'
+                                alt='review'
+                            />
+                            <Image
+                                height={'auto'}
+                                width={'auto'}
+                                src={ghassanKhoulaghasi}
+                                className='rounded-full object-cover aspect-square w-12 h-12'
+                                alt='review'
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex gap-5 flex-col">
+                    <div className="flex items-center w-full mt-7">
+                        {/* <h2 className="text-xl">Complete</h2>
+                        <HiDotsHorizontal className="ml-auto mr-20 text-mute text-2xl" /> */}
+                    </div>
+                    <div className="bg-white shadow-xl rounded-lg p-5 flex flex-col gap-3">
+                        <span className="bg-purple-300 w-full h-2 rounded-xl" />
                         <h3 className="text-2xl">Date of Infographic</h3>
                         <p className="text-mute text-xl">July 14</p>
                         <div className="flex gap-2">
