d3a34dbc1d3e0aec9a1314bb031f667aa9acad4b
Faraax <suny.faraz@gmail.com>
2023-10-06 01:36:47 +0500

    adjusted sidebar fonts

--- app/components/SidebarList.jsx
+++ app/components/SidebarList.jsx
@@ -7,7 +7,7 @@ export default function SidebarList({ list }) {
     return (
         <li className={`text-xl text-mute flex gap-4 ${pathname === list.url ? "text-secondary" : ''} hover:text-secondary duration-200 cursor-pointer`}>
             {list.icon}
-            <Link href={list.url} className='text-sm text-center flex items-center justify-center font-thin'>
+            <Link href={list.url} className='text-sm text-center flex items-center justify-center'>
                 {list.title}
             </Link>
         </li>
